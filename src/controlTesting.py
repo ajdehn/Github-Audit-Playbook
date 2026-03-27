368721839495404c1370e29ae808166de2d42d39
AJ Dehn <ajdehn76@gmail.com>
2026-03-27 17:33:03 -0500

    Report formatting update.

--- src/controlTesting.py
+++ src/controlTesting.py
@@ -72,7 +72,7 @@ def test_branch_protection_rules(audit):
         "An active ruleset OR branch protection rule is in enforced on the repository.",
         "Pull requests merging into the 'main' branch require at least one approval."
     ]
-    table_headers = ["Sample Number", "Repository Name", "Conclusion", "Comments"]
+    table_headers = ["Repository Name", "Conclusion", "Comments"]
     ctrl = Control(ctrl_id, ctrl_desc, test_procedures, test_attributes, table_headers = table_headers)
 
     # Get list of all repositories.
