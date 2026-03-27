bbd7645da528bc0d24f2a346bd8301e2ecb05b6a
ajdehn <ajdehn76@gmail.com>
2026-03-27 08:16:27 -0500

    Create runAudit.py

---
+++ src/runAudit.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import controlTesting
+import random
+import os
+from utils import generate_pdf_report
+
+class Audit:
+    """
+    Contains audit metadata.
+    """
+    def __init__(self, org_name, start_date, end_date, sample_size, gh_token, evidence_folder="audit_evidence"):
+        """
+        Initializes the audit instance with the provided four attributes.
+        """
+        self.gh_token = gh_token                # Authentication token to analyze the Github environment.
+        self.org_name = org_name                # Github organization name (Ex. AuditOps)
+        self.start_date = start_date            # Start date of the audit period (YYYY-MM-DD)
+        self.end_date = end_date                # Final date of the audit period (YYYY-MM-DD)
+        self.sample_size = sample_size          # Maxiumum number of samples per repo (Ex. 5)
+        self.evidence_folder = evidence_folder  # Name of the evidence_folder
+
+if __name__ == "__main__":
+    # Load variables from .env file
+    load_dotenv()
+    audit = Audit(os.getenv("org_name"), os.getenv("start_date"), os.getenv("end_date"), 
+    int(os.getenv("samples_per_repo")), os.getenv("github_token"))
+
+    controls = []
+
+    # Test branch protection rules
+    control_results = controlTesting.test_branch_protection_rules(audit)
+    print(control_results)
+    controls.append(control_results)
+
+    # Test change approvals
+    controls.append(controlTesting.test_change_approvals(audit))
+
+    generate_pdf_report(audit, controls, "github_audit_report.pdf")
