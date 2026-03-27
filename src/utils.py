3328bc6155839dae027ed81c1c42f42dfa948824
AJ Dehn <ajdehn76@gmail.com>
2026-03-27 10:34:04 -0500

    add confirmDeleteFolder

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,24 @@
 import json
 import os
+import shutil
 import requests
 from datetime import datetime
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, LETTER
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, ListFlowable, ListItem
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+def confirmDeleteFolder(folder_path):
+    if os.path.exists(folder_path):
+        confirm = input(f"Folder '{folder_path}' exists. Do you want to delete it? (y/N): ").strip().lower()
+        
+        if confirm == "y":
+            shutil.rmtree(folder_path)
+            print("Deleting old evidence folder.")
+        elif confirm == "N":
+            print("Using cached evidence.")
+        else:
+            print("Aborted. Folder not deleted.")
 
 """
     Saves a json file to a specified path
@@ -99,10 +112,12 @@ def generate_pdf_report(audit, controls, filename="github_audit_report.pdf"):
 
     # Add controls to the report
     for control in controls:
-        # Populate Control Description, Test Procedures, and Test Attributes
+        # Populate Control Description, Conclusion, Test Procedures, and Test Attributes
         elements.append(Paragraph(f"<b>Control Description:</b> {control.ctrl_desc}", styles["Normal"]))
         elements.append(Spacer(1, 6))
 
+        elements.append(Paragraph(f"<b>Conclusion:</b> {"Pass" if control.result else "Fail"}", styles["Normal"]))
+        elements.append(Spacer(1, 6))
 
         # Add test procedures (ordered list) to the report
         elements.append(Paragraph(f"<b>Test Procedures:</b>", styles["Normal"]))
@@ -120,55 +135,56 @@ def generate_pdf_report(audit, controls, filename="github_audit_report.pdf"):
         elements.append(ListFlowable(test_attribute_flowable_items, bulletType='bullet'))
         elements.append(Spacer(1, 12))
 
-        # Build audit results table
-        table_data = []
-        table_header_style = ParagraphStyle(name='TableHeader', fontSize=9, leading=11, alignment=1, spaceAfter=0)
-        table_cell_style = ParagraphStyle(name='TableCell', fontSize=8, leading=10)
-        
-        headers_wrapped = [Paragraph(h, table_header_style) for h in control.table_headers]
-        table_data.append(headers_wrapped)
-        # Populate rows into audit results table
-        sample_number = 1 # Only used if include_sample_number is True
-        for sample in control.samples:
-            wrapped_row = []
-            if control.include_sample_number:
-                wrapped_row.append(sample_number)            
-            for item in sample.sample_id:
-                wrapped_row.append(Paragraph(str(sample.sample_id.get(item)), table_cell_style))
+        if control.table_headers:
+            # Build audit results table
+            table_data = []
+            table_header_style = ParagraphStyle(name='TableHeader', fontSize=9, leading=11, alignment=1, spaceAfter=0)
+            table_cell_style = ParagraphStyle(name='TableCell', fontSize=8, leading=10)
             
-            if sample.result:
-                wrapped_row.append("Pass")
-            else:
-                control.result = False
-                fail_text = "Fail"
-                # Highlight text when change fails.     
-                wrapped_row.append(Paragraph(f'<font color="red">{fail_text}</font>', styles["Normal"]))
-                # Add comments when sample fails
-                wrapped_row.append(Paragraph(str(sample.comments), table_cell_style))
-            table_data.append(wrapped_row)
-            sample_number = sample_number + 1
+            headers_wrapped = [Paragraph(h, table_header_style) for h in control.table_headers]
+            table_data.append(headers_wrapped)
+            # Populate rows into audit results table
+            sample_number = 1 # Only used if include_sample_number is True
+            for sample in control.samples:
+                wrapped_row = []
+                if control.include_sample_number:
+                    wrapped_row.append(sample_number)            
+                for item in sample.sample_id:
+                    wrapped_row.append(Paragraph(str(sample.sample_id.get(item)), table_cell_style))
+                
+                if sample.result:
+                    wrapped_row.append("Pass")
+                else:
+                    control.result = False
+                    fail_text = "Fail"
+                    # Highlight text when change fails.     
+                    wrapped_row.append(Paragraph(f'<font color="red">{fail_text}</font>', styles["Normal"]))
+                    # Add comments when sample fails
+                    wrapped_row.append(Paragraph(str(sample.comments), table_cell_style))
+                table_data.append(wrapped_row)
+                sample_number = sample_number + 1
 
-        # Make table width = page width minus margins
-        table_width = page_width - 2 * 72  # assuming 1-inch margins
-        col_width = table_width / len(table_data[0])  # divide evenly across columns
-        col_widths = [col_width] * len(table_data[0])
-        # Create table
-        table = Table(table_data, colWidths=col_widths)
-        table.hAlign = "LEFT"
-        # Style the table
-        style = TableStyle([
-            ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-            ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-            ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-            ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-            ("BOTTOMPADDING", (0, 0), (-1, 0), 10),
-            ("GRID", (0, 0), (-1, -1), 1, colors.black),
-            ("VALIGN", (0, 0), (-1, -1), "TOP"),        
-        ])
-        table.setStyle(style)
-        elements.append(table)
-        # Add space after the table.
-        elements.append(Spacer(1, 20))
+            # Make table width = page width minus margins
+            table_width = page_width - 2 * 72  # assuming 1-inch margins
+            col_width = table_width / len(table_data[0])  # divide evenly across columns
+            col_widths = [col_width] * len(table_data[0])
+            # Create table
+            table = Table(table_data, colWidths=col_widths)
+            table.hAlign = "LEFT"
+            # Style the table
+            style = TableStyle([
+                ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+                ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+                ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
+                ("BOTTOMPADDING", (0, 0), (-1, 0), 10),
+                ("GRID", (0, 0), (-1, -1), 1, colors.black),
+                ("VALIGN", (0, 0), (-1, -1), "TOP"),        
+            ])
+            table.setStyle(style)
+            elements.append(table)
+            # Add space after the table.
+            elements.append(Spacer(1, 20))
 
     # Build PDF
     doc.build(elements)
